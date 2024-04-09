fa39b0580309bd7e65b81d423e823dda0713dc81
Hrushikesh Patel <hrushikesh592002@gmail.com>
2024-04-10 04:09:23 +0530

    updated coupons page

--- cmsfrontend/src/pages/CouponByCompanyPage.jsx
+++ cmsfrontend/src/pages/CouponByCompanyPage.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-// import * as React from 'react';
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
-// import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-// import TableContainer from '@mui/material/TableContainer';
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
@@ -12,25 +9,6 @@ import { TableBody, TableContainer, TextField, Button } from "@mui/material";
 import "./CouponByCompanyPage.css";
 import CouponCard from "../components/CouponCard";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
 const CouponByCompanyPage = () => {
   const [couponArr, setCouponArr] = useState([
     {
@@ -66,7 +44,7 @@ const CouponByCompanyPage = () => {
   const [companyId, setCompanyId] = useState(0);
 
   const handleSubmit = () => {
-    console.log("handle");
+    console.log("search coupons from company page");
     // axios.get(""+companyId).then((response)=>{console.log(response)}).catch((error)=>{console.log(error)});
   };
 
